589ee395f69a4c377dbd7c766105fb87e936ab5d
Zach Timpe <zacharytimpe@gmail.com>
2024-12-05 08:55:03 -0800

    Add tableau interface

--- app.R
+++ app.R
@@ -3,6 +3,8 @@ library(bslib)
 library(readxl)
 library(DT)
 library(dplyr)
+library(GWalkR)
+library(shinyjs)
 options(shiny.maxRequestSize = 10 * 1024^2)
 ui <-  page_navbar(
     title = "Breast Cancer Data Viewer",
@@ -19,80 +21,66 @@ ui <-  page_navbar(
                     ".xlsx"
                   )
         ),
+        uiOutput('xlsx_options'),
+        actionButton('submit','Submit'),
+        actionButton('clear','Clear'),
         helpText("Please upload an Excel file (.xls or .xlsx)")
       ),
-      card(
-        card_header ( 'A glance at the uploaded dataset'),
-        card_body(
-          uiOutput('orig_data')
-        )
-      )
-      )
-    ),
-    nav_panel('Drilldown',
-      layout_sidebar(
-        sidebar = list(
-          card(
-            card_header('Select a subset of sites'),
-            card_body(checkboxGroupInput(
-        'sites', # input reference name
-        label = '',
-        choices = NULL, # gets filled when upload a dataset
-        selected = NULL,
-        inline = TRUE)
-            ) # end card body
-          ), #end card
-      card(
-        card_header('Year'),
-        card_body(
-          checkboxGroupInput('year',
-                   label = '',
-                   selected = NULL, 
-                   choices = list('Baseline'='0','Year 1'='1'),
-                   inline = TRUE)
-                   ) #end card body
-          ), # end card
-          varSelectizeInput('selected_vars1',label = 'Select variables to view',data = NULL,multiple = TRUE)
-      ),
-    card(
-      card_header('Filtered data shown here'),
-      card_body(uiOutput('filtered_data'))
+      gwalkrOutput('interactive_panel')
     )
-    )
-    )
+)
 )
 
 
 server <- function(input, output,session) {
   # Reactive expression to read the Excel file
-  data <- reactive({
+  data <- reactiveVal(NULL)
+  
+  observeEvent(input$submit,{
     req(input$file)
     
-    # Read the file
-    tryCatch(
-      {
-        ext <- tools::file_ext(input$file$name)
-        switch(ext,
-               RDS = readRDS(input$file$datapath),
-               xlsx = read_excel(input$file$datapath),
-               validate("Invalid file; Please upload a .xlsx or .rds file")
-        )
-    },
-      error = function(e) {
-        stop("Error reading the file. Please make sure it's a valid Excel or RDS file.")
-      }
-    )
+    file <- input$file$datapath
+    file_ext <- tools::file_ext(input$file$name)
+    
+    tryCatch({
+      data(switch(file_ext,
+                  "xlsx" = {
+                    req(input$header_row)
+                    read_excel(file, skip = as.numeric(input$header_row) - 1)
+                  },
+                  "RDS" = readRDS(file),
+                  stop("Unsupported file type")
+      ))
+    }, error = function(e) {
+      showNotification("Error reading the file. Please make sure it's a valid Excel or RDS file.", type = "error")
+      data(NULL)
+    })
   })
-  observeEvent(data(),{
+  
+  output$xlsx_options <- renderUI({
     req(input$file)
-    freezeReactiveValue(input,'sites')
-    choices <- unique(data()$site)
-    updateCheckboxGroupInput(session,
-                             'sites',
-                             choices = choices, 
-                             selected = choices)
-
+    if(grepl('\\.xlsx$',input$file$name)){
+      numericInput('header_row','Row number for column names',value = 1, min = 1)
+    }
   })
+  
+  observeEvent(input$clear,{
+    data(NULL)
+    unlink(input$file$datapath)
+    reset(id = '')
+    updateNumericInput(session,'header_row',value = 1)
+  })
+  
+  # observeEvent(data(),{
+  #   req(input$file)
+  #   freezeReactiveValue(input,'sites')
+  #   choices <- unique(data()$site)
+  #   updateCheckboxGroupInput(session,
+  #                            'sites',
+  #                            choices = choices, 
+  #                            selected = choices)
+  # 
+  # })
   filtering_variables <- reactive({
     req(input$file)
     setdiff(names(data()),c('site','year'))
@@ -104,58 +92,63 @@ server <- function(input, output,session) {
   #                     choices = filtering_variables())
   # })
   
-  output$orig_data <- renderUI({
-    data()|>
-      filter(row_number() < 6)|>
-      DT::datatable(rownames=FALSE,
-                    options = list(scrollX = TRUE, 
-                                   scrollY = TRUE,
-                                   pageLength = 10, 
-                                   dom = 'ti',
-                      columnDefs = list(
-                                        list(className = 'dt-center', 
-                                             targets = '_all')
-                                                  )),
-                    filter = list(position = 'top', clear = FALSE)
-      )
-  })
+  # output$orig_data <- renderUI({
+  #   data()|>
+  #     filter(row_number() < 6)|>
+  #     DT::datatable(rownames=FALSE,
+  #                   options = list(scrollX = TRUE, 
+  #                                  scrollY = TRUE,
+  #                                  pageLength = 10, 
+  #                                  dom = 'ti',
+  #                     columnDefs = list(
+  #                                       list(className = 'dt-center', 
+  #                                            targets = '_all')
+  #                                                 )),
+  #                   filter = list(position = 'top', clear = FALSE)
+  #     )
+  # })
    
-  observeEvent(input$file,{
-     updateVarSelectizeInput(session = session,
-                         'selected_vars1',
-                         data = data(),
-                         selected = names(data())[1:5]
-                         )
-  })
+  # observeEvent(input$file,{
+  #    updateVarSelectizeInput(session = session,
+  #                        'selected_vars1',
+  #                        data = data(),
+  #                        selected = names(data())[1:5]
+  #                        )
+  # })
 
 
-  filtered_data <- reactive({
-   tmp <- data() |>
-      select(site,!!!input$selected_vars1)|>
-      filter(site %in% input$sites)
-   if(nrow(tmp) > 1000) {
-     tmp <- tmp[sample(nrow(tmp),size = 500),]
-     }
-    
-   tmp
-  })
-
-  output$filtered_data <- renderUI({
-      req(filtered_data())
-      filtered_data()|>
-         DT::datatable(rownames=FALSE,
-                     options = list(scrollX = TRUE,
-                                    scrollY = TRUE,
-                                    pageLength = 5,
-                                    lengthMenu = c(5,10,25,50),
-                                    dom = 'ltip'
-                                    #columnDefs = list(
-                                     # list(className = 'dt-center',
-                                      #     targets = '_all')
-                                    ),
-                     filter = list(position = 'top', clear = FALSE)
-       )
-
-  })
+  # filtered_data <- reactive({
+  #  tmp <- data() |>
+  #     select(site,!!!input$selected_vars1)|>
+  #     filter(site %in% input$sites)
+  #  if(nrow(tmp) > 1000) {
+  #    tmp <- tmp[sample(nrow(tmp),size = 500),]
+  #    }
+  #   
+  #  tmp
+  # })
+  # 
+  # output$filtered_data <- renderUI({
+  #     req(filtered_data())
+  #     filtered_data()|>
+  #        DT::datatable(rownames=FALSE,
+  #                    options = list(scrollX = TRUE,
+  #                                   scrollY = TRUE,
+  #                                   pageLength = 5,
+  #                                   lengthMenu = c(5,10,25,50),
+  #                                   dom = 'ltip'
+  #                                   #columnDefs = list(
+  #                                    # list(className = 'dt-center',
+  #                                     #     targets = '_all')
+  #                                   ),
+  #                    filter = list(position = 'top', clear = FALSE)
+  #      )
+  # 
+  # })
+  output$interactive_panel <- renderGwalkr({
+    req(data())
+    gwalkr(data())
+  }
+  )
  }
 shinyApp(ui,server)
